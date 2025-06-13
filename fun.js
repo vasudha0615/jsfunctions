98f4ad62ebe7f238b2d4b3e8b6dae7103d096bcb
vasudha0615 <tullavasudha@gmail.com>
2025-06-13 04:37:02 +0000

    functionsadded

---
+++ fun.js
@@ -0,0 +1,27 @@
+// functions are one of the most powerful features 
+// functions are used always like declaration + call 
+//syntax
+//function declaration
+sayHello()
+function sayHello(){
+    console.log("hello")
+}
+//function calling
+sayHello()
+// this is a js hoisting
+
+
+//functions with parameters
+function sum(num1,num2,num3){
+    console.log(num1+num2+num3)
+} sum(10,5,2)
+
+function sub(num4,num5){
+    console.log(num4-num5)
+}sub(25,8)
+function mul(num6,num7){
+    console.log(num6*num7)
+}mul(1,17)
+function divide(num8,num9){
+    console.log(num8/num9)
+}divide(17,1)
